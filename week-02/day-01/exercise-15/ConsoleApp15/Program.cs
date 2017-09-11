f42cd7ae6e81dbd08b615ad307de1a31fa6927f8
Flora <floraballabas@gmail.com>
2017-09-11 17:01:54 +0200

    Uploaded task15

---
+++ week-02/day-01/exercise-15/ConsoleApp15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("How many kms can you run?");
+            int km = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wow, that is " + km * 0.62 + "miles");
+            Console.ReadLine();
+        }
+    }
+}
