c38d740ede3eaa1b53e16f3358c23343283da4b4
Flora <floraballabas@gmail.com>
2017-09-11 17:15:25 +0200

    Uploaded task18

---
+++ week-02/day-01/exercise-18/ConsoleApp18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            Console.WriteLine("Write a number");
+            int input = int.Parse(Console. ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("Your number is even!");
+            }
+            else {
+                Console.WriteLine("Your number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
