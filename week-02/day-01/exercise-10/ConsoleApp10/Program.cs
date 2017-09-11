f8c9fb8d29fca50cd1d1fc6940339ff094f88644
Flora <floraballabas@gmail.com>
2017-09-11 15:24:38 +0200

    Uploaded task10

---
+++ week-02/day-01/exercise-10/ConsoleApp10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable than print their values
+            // Your name as a string
+            string name = "Flóra";
+            // Your age as an integer
+            int age = 24;
+            // Your height in meters as a double
+            double height = 1.62;
+            // Wether you are married or not as a boolean
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+
+        }
+    }
+}
