0c3ae15ffc138b3272315bf49f5913e31514432d
Flora <floraballabas@gmail.com>
2017-09-11 15:14:58 +0200

    Uploaded task9

---
+++ week-02/day-01/exercise-09/ConsoleApp9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 50;
+            double heightInM = 1.62;
+
+            Console.WriteLine(massInKg /= heightInM *= heightInM);
+        }
+    }
+}
