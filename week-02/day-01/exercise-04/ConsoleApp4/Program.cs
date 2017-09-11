79e8507c0cc3c538c2c41850c0e57b4b5490e4a0
Flora <floraballabas@gmail.com>
2017-09-11 13:49:58 +0200

    Added gitignore, and uploaded task4

---
+++ week-02/day-01/exercise-04/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Flóra Ballabás");
+            Console.WriteLine("24");
+            Console.WriteLine("1.62");
+        }
+    }
+}
