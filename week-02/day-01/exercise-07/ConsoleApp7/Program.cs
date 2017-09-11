f1cc51d26927ccdbfd7982ab4732b677e9d62033
Flora <floraballabas@gmail.com>
2017-09-11 14:38:36 +0200

    Uploaded task7

---
+++ week-02/day-01/exercise-07/ConsoleApp7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favnumber = 8;
+            Console.WriteLine("My favourite number is: " + favnumber);
+        }
+    }
+}
