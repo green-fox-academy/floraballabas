997187a01b04e0266945ba9fe558d1a3c86b87fc
Flora <floraballabas@gmail.com>
2017-09-11 14:01:46 +0200

    Uploaded task5

---
+++ week-02/day-01/exercise-05/ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 + 22);
+            Console.WriteLine(22 - 13);
+            Console.WriteLine(22 * 13);
+            Console.WriteLine(22 / 13.0);
+            Console.WriteLine(22 / 13);
+            Console.WriteLine(22 % 13);
+        }
+    }
+}
