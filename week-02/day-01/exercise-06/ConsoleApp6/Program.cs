d2f2d11a08d84a8d57fee2a0530cf2e5e4f6303c
Flora <floraballabas@gmail.com>
2017-09-11 14:23:37 +0200

    Uploaded task6

---
+++ week-02/day-01/exercise-06/ConsoleApp6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(6 * 85);
+            Console.WriteLine(884 / 2856.0);
+        }
+    }
+}
