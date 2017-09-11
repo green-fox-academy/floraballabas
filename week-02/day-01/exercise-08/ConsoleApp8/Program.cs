dcbf2b0a3d149d7d72ae80fd4c7bdb0e3cefbabf
Flora <floraballabas@gmail.com>
2017-09-11 14:45:33 +0200

    Uploaded task8

---
+++ week-02/day-01/exercise-08/ConsoleApp8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 526;
+
+            a = b;
+            b = a - 403;
+            Console.WriteLine(b);
+        }
+    }
+}
