9b960f20bbd39fedaadcae3245f6028d2b9b95b5
Flora <floraballabas@gmail.com>
2017-09-11 16:32:15 +0200

    Uploaded task13

---
+++ week-02/day-01/exercise-13/ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 16;
+            int currentMinutes = 22;
+            int currentSeconds = 30;
+
+            int remainingHours = 24 - currentHours;
+            int remaingMinutes = 59 - currentMinutes;
+            int remainingSeconds = 59 - currentSeconds;
+
+            Console.WriteLine(remainingHours * 3600 + remaingMinutes *60 + remainingSeconds);
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented bt the variables
+        }
+    }
+}
