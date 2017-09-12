6c557ab9d9b9e8b3f2bf2c077926ce43e6029379
Flora <floraballabas@gmail.com>
2017-09-12 13:33:17 +0200

    Uploaded the Greeter function

---
+++ week-02/day-02/exercise-02/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+    
+        public static void Greet()
+        {
+            string al = "Greenfox";
+            Console.WriteLine("Greetings dear, "+al);
+        }
+        static void Main(string[] args)
+        {
+            Greet();
+            Console.ReadLine();
+        }
+    }
+}
