9a80fc9f1fd4c38d92e52ff3e8772b7ae7275975
Flora <floraballabas@gmail.com>
2017-09-12 13:19:25 +0200

    Uploaded the Doubling exercise

---
+++ week-02/day-02/exercise-01/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Doubling()
+        {
+            int ak = 123;
+            ak *= 2;
+            Console.WriteLine(ak);
+        }
+
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            Doubling();
+            Console.ReadLine();
+        }
+    }
+}
