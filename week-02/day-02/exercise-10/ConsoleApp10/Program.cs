850376e00216a6a795874307c4b3f9682ffa9fa0
Flora <floraballabas@gmail.com>
2017-09-12 22:00:08 +0200

    Uploaded the ChangeElement exercise

---
+++ week-02/day-02/exercise-10/ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `s`
+            //   with the following content: `[1, 2, 3, 8, 5, 6]`
+            int[] s = {1, 2, 3, 8, 5, 6};
+            // - Change the 8 to 4
+            s[3] = 4;
+            // - Print the fourth element
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+        }
+    }
+}
