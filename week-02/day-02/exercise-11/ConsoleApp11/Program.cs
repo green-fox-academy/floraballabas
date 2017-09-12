b36c6f7b55efc3f2bca4d1d7d31d4daf52f77c5d
Flora <floraballabas@gmail.com>
2017-09-12 22:17:30 +0200

    Uploaded the Increment Eelement exercise

---
+++ week-02/day-02/exercise-11/ConsoleApp11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            int[] t = {1, 2, 3, 4, 5};
+            //- Increment the third element
+            for (int i = 1; i < 2; i++)
+            {
+                t[3] += i + 1;
+            }
+            //- Print the third element
+            Console.WriteLine(t[3]);
+            Console.ReadLine();
+        }
+    }
+}
