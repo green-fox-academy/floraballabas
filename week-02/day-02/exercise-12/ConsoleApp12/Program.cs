748d66706ce7c8fd3cb26857f793b294fb1821f4
Flora <floraballabas@gmail.com>
2017-09-12 22:24:36 +0200

    Uploaded the Print Elements exercise

---
+++ week-02/day-02/exercise-12/ConsoleApp12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            int[] af = {4, 5, 6, 7};
+            // - Print all the elements of `af`
+            foreach (int number in af)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
