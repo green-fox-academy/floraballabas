56a2da74551e3e10462a9fb5c8b93de4fc040dfd
Flora <floraballabas@gmail.com>
2017-09-12 17:40:07 +0200

    Uploaded the Printer exercise

---
+++ week-02/day-02/exercise-06/ConsoleApp6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            string[] parameter = { "first", "second", "third" };
+            Printer(parameter);
+        }
+
+        public static void Printer(string[] parameter)
+        {
+            for (int i = 0; i < parameter.Length; i++)
+            {
+
+                Console.WriteLine(parameter[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
