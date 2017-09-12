79c010275bc4339e00c06d12128e2b1d99b54310
Flora <floraballabas@gmail.com>
2017-09-12 16:38:21 +0200

    Uploaded the Sum exercise

---
+++ week-02/day-02/exercise-04/ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        public static int Sum(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                n += i - 1;
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(5));
+
+            Console.ReadKey();
+        }
+    }
+}
