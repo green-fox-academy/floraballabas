ec72f269b5e38cd87ec46ce63f593ab2a1a56fb7
Flora <floraballabas@gmail.com>
2017-09-12 21:41:58 +0200

    Uploaded the SumElements exercise

---
+++ week-02/day-02/exercise-09/ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            int[] r = {54, 23, 66, 12};
+            // - Print the sum of the second and the third element
+            Console.WriteLine(r[1]+r[2]);
+            Console.ReadLine();
+        }
+    }
+}
