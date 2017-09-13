a69f40755bd36e461e509577fa6bc79d014a511c
Flora <floraballabas@gmail.com>
2017-09-13 14:51:40 +0200

    Uploaded the Reverse task

---
+++ week-02/day-03/exercise-05/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public static void Reverse()
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            char[] reversedarray = reversed.ToCharArray();
+            Array.Reverse(reversedarray);
+            string output = new string(reversedarray);
+            Console.WriteLine(output);
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+            Reverse();
+            Console.ReadLine();
+        }
+    }
+}
