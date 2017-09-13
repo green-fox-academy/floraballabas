27152eb2579ef03dbc38f85a43508f8baf1b0317
Flora <floraballabas@gmail.com>
2017-09-13 13:49:35 +0200

    Uploaded the Todo Print task

---
+++ week-02/day-03/exercise-04/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+            string todoText = "My todo:";
+            todoText = todoText.Insert(8, "\n - Download games");
+            string indent = "   ";
+            todoText = todoText.Insert(26, "\n"+indent+" - Diablo");
+
+
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
