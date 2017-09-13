d3771a9f2e858d7b39ea1f5edc71c26ac561501b
Flora <floraballabas@gmail.com>
2017-09-13 15:12:52 +0200

    Uploaded the SolarSystem task

---
+++ week-02/day-03/exercise-06/ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+            planetList.Insert(5, "Saturn");
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
