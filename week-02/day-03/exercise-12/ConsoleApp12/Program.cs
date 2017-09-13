4bfffbfbaa562ee26c5a34577ca6398d93a2c710
Flora <floraballabas@gmail.com>
2017-09-13 17:26:53 +0200

    Uploaded the QuoteSwap task

--- week-02/day-03/exercise-12/ConsoleApp12/Program.cs
+++ week-02/day-03/exercise-12/ConsoleApp12/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApp12
+namespace QuoteSwap
 {
-    class Program
+    public class QuoteSwap
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            string temp = list[2];
+            list[2] = list[5];
+            list[5] = temp;
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is 
+
+
+            //to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+            foreach (string output in list)
+            {
+                Console.Write(output+" ");
+            }
+            Console.ReadLine();
         }
     }
 }
