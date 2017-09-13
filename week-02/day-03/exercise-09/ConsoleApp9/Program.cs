1a19535ccc41cf49d32f3c691367252c263f1bc6
Flora <floraballabas@gmail.com>
2017-09-13 16:53:21 +0200

    Uploaded the CandyShop task

---
+++ week-02/day-03/exercise-09/ConsoleApp9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CandyShop
+{
+    public class Candyshop
+    {
+        public static void Main(string[] args)
+        {
+            // Accidentally we added "2" and "false" to the list. 
+            // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+            // No, don't just remove the lines
+
+            var list = new List<object>();
+            list.Add("Cupcake");
+            list.Add(2);
+            list.Add("Brownie");
+            list.Add(false);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i == 1)
+                {
+                    list[i] = "Croissant";
+                }
+                if (i == 3)
+                {
+                    list[i] = "Ice Cream";
+                }
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
