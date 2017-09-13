d3103c113a04b490ede0c650fc7ecbb6929a2938
Flora <floraballabas@gmail.com>
2017-09-13 16:03:53 +0200

    Uploaded the Matchmaking task

---
+++ week-02/day-03/exercise-07/ConsoleApp7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            // Join the two lists by matching one girl with one boy in the order list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            var order = new List<string>();
+
+            for (int i = 0; i < girls.Count; i++)
+            {
+                order.Add(girls[i]);
+                order.Add(boys[i]);
+            }
+
+            if (girls.Count + boys.Count != order.Count)
+            {
+                order.Add(boys[5]);
+            }
+
+            foreach (var or in order)
+            {
+                Console.WriteLine(or);
+            }
+            Console.ReadLine();
+        }
+    }
+}
