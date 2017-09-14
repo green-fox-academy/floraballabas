9e72669bbf401a7014bda7d1513e2729e146398d
Flora <floraballabas@gmail.com>
2017-09-14 10:55:25 +0200

    Uploaded the PrintBigger task - solved with if statement - this is actually the 20th exercise, not the 21st

---
+++ week-02/day-01/exercise-21/ConsoleApp21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+
+            Console.WriteLine("Please write a number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please write a second number");
+            int input1 = int.Parse(Console.ReadLine());
+
+            if (input > input1)
+            {
+                Console.WriteLine("The bigger number is " + input);
+            }
+            else
+            {
+                Console.WriteLine("The bigger number is " + input1);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
