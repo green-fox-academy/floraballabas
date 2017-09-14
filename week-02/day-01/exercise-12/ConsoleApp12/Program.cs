dc7c9da7889137abc6ced9a5cb647bfa51d3b598
Flora <floraballabas@gmail.com>
2017-09-14 10:16:28 +0200

    Uploaded the Cuboid task solved with arithmetic operators

--- week-02/day-01/exercise-12/ConsoleApp12/Program.cs
+++ week-02/day-01/exercise-12/ConsoleApp12/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace ConsoleApp12
+namespace GreenFox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double height = 5;
+            double width = 6;
+            double length = 8;
+
+            double surfaceArea = 2 * (length * width + width * height + height * length);
+            double volume = length * width * height;
+
+            Console.WriteLine("Surface Area: "+surfaceArea);
+            Console.WriteLine("Volume: "+volume);
         }
     }
 }
