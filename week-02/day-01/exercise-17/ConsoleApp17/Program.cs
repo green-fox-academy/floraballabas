acdcbffb77e2c45dcf7ed5b67961c0923b90ced5
Flora <floraballabas@gmail.com>
2017-09-14 10:43:10 +0200

    Uploaded the AvarageOfInputs

---
+++ week-02/day-01/exercise-17/ConsoleApp17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+            Console.WriteLine("Please give a number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give a number");
+            int input1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give a number");
+            int input2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give a number");
+            int input3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give a number");
+            int input4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum: " + (input+input1+input2+input3+input4));
+            Console.WriteLine("Avarage: " + (input + input1 + input2 + input3 + input4) / 5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
