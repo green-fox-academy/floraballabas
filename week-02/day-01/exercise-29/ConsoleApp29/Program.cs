3082b9b3a3d9222ec60c9a31a41c89a155cd8059
Flora <floraballabas@gmail.com>
2017-09-14 15:40:22 +0200

    Modified the Pyramid task with Console.WriteLine to start a new line

--- week-02/day-01/exercise-29/ConsoleApp29/Program.cs
+++ week-02/day-01/exercise-29/ConsoleApp29/Program.cs
@@ -27,7 +27,7 @@ namespace GreenFox
                 
                 for (int j = 1; j <= 2 * i - 1; j++)
                     Console.Write("*");
-                    Console.Write("\n");
+                    Console.WriteLine();
             }
 
                 Console.ReadLine();
