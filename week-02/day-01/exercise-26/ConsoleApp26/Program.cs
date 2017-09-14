d19ee93bc91d6898d59bba7401d1d8b33d012fd0
Flora <floraballabas@gmail.com>
2017-09-14 12:00:35 +0200

    Solved the CountFromTo with a for loop

---
+++ week-02/day-01/exercise-26/ConsoleApp26/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that asks for two numbers
+            // If the second number is not bigger than the first one it should print:
+            // "The second number should be bigger"
+            //
+            // If it is bigger it should count from the first number to the second by one
+            //
+            // example:
+            //
+            // first number: 3, second number: 6, should print:
+            //
+            // 3
+            // 4
+            // 5
+            Console.WriteLine("Please give a number");
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please give a second number");
+            int input1 = int.Parse(Console.ReadLine());
+
+            if (input > input1)
+            {
+                Console.WriteLine("The second number should be bigger");
+            }
+            else
+            {
+                for (int i = input; i < input1; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
