4768ccbf7a0fb0e0a30b5674f620a87c8185061b
Flora <floraballabas@gmail.com>
2017-09-14 15:41:05 +0200

    Solved the GuessTheNumber game with a while loop

---
+++ week-02/day-01/exercise-33/ConsoleApp33/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+            int number = 8;
+            //Console.WriteLine("Try to guess which number I am thinking of");
+            //int input = int.Parse(Console.ReadLine());
+            int input = 0;
+
+            Console.WriteLine("Try to guess which number I am thinking of");
+
+            while (input != number) 
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input > number)
+                {
+                    Console.WriteLine("The stored number is lower");
+                }
+                else if (input < number)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+                Console.WriteLine("Try to guess which number I am thinking of");
+            }
+            if (input == number)
+            {
+                Console.WriteLine("You found the number: 8");
+            }
+            Console.ReadLine();
+        }
+    }
+}
