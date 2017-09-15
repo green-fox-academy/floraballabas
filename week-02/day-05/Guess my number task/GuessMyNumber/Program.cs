daea1b828232ae5541b7f4954066b3b1fd8787ea
Flora <floraballabas@gmail.com>
2017-09-15 11:48:04 +0200

    Solved the Guess my NUmber task without optional lives

---
+++ week-02/day-05/Guess my number task/GuessMyNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program where the program chooses a number between 1 and 100.The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
+            //Make the range customizable(ask for it before starting the guessing).
+            //You can add lives. (optional)
+
+            Console.WriteLine("Let's play Guess my number! Give me where should the range start");
+            int range1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("And what should be the top range?");
+            int range2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Allright, I will think of a number between "+range1+" and "+range2);
+            Random random = new Random();
+            int guessNumber = random.Next(range1, range2);
+            Console.WriteLine("Let's start. Make your guess!");
+            int input = 0;
+
+            while (input != guessNumber)
+            {
+                input = int.Parse(Console.ReadLine());
+                if (input > guessNumber)
+                {
+                    Console.WriteLine("My number is lower");
+                }
+                else if (input < guessNumber)
+                {
+                    Console.WriteLine("My number is higher");
+                }
+                Console.WriteLine("Try to guess which number I am thinking of");
+            }
+            if (input == guessNumber)
+            {
+                Console.WriteLine("You found the number: "+guessNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-05/Palindrome Searcher task/PalindromeSearcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromeSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
