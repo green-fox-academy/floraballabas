a55d774572be2c753dcb82f97feac9afd8f98675
Flora <floraballabas@gmail.com>
2017-09-16 22:59:13 +0200

    Uploaded the DrawDiagonal task

---
+++ week-02/day-01/exercise-32/DrawDiagonal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+            Console.WriteLine("Please tell me how many lines the square should have");
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+                Console.Write("%");
+            Console.WriteLine();
+
+            for (int i = 0; i < lines - 2; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                    Console.Write("%");
+                for (int j = 0; j < lines - 2; j++)
+                {
+                    if (j == i)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                }
+                for (int j = 0; j < 1; j++)
+                    Console.Write("%");
+                
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < lines; i++)
+                Console.Write("%");
+
+            Console.ReadLine();
+
+        }
+    }
+}
