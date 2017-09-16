16d33e675d2b9cf9725c1f1c9aa21e26bee5d78c
Flora <floraballabas@gmail.com>
2017-09-16 19:37:17 +0200

    Solved the Draw a Diamond task

---
+++ week-02/day-01/exercise-30/DrawADiamond/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+            Console.WriteLine("Please enter a number");
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= ((lines/2)+1); i++)
+            {
+                for (int j = 0; j < lines - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < i*2-1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+            for (int i = lines/2; i >= 0; i--)
+            {
+                for (int j = 0; j < lines - i; j++)
+                    Console.Write(" ");
+                for (int j = 0; j < i * 2 - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
