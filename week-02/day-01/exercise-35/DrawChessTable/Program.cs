6200dacc8c2a85819e1654f64567f806fa9b4dee
Flora <floraballabas@gmail.com>
2017-09-16 23:16:01 +0200

    Uploaded the DrawChessTable task

---
+++ week-02/day-01/exercise-35/DrawChessTable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+            int table = 8;
+            for (int i = 1; i <= table; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < 1; j++)
+                        Console.Write(" ");
+                    for (int j = 0; j < 4; j++)
+                        Console.Write("%");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int j = 0; j < 4; j++)
+                        Console.Write("%");
+                    Console.WriteLine();
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
