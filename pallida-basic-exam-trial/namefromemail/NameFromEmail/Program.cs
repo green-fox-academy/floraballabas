a776240a0a75c4a22e83068f656ced149b8d0267
Flora <floraballabas@gmail.com>
2017-09-18 12:03:44 +0200

    Created the NameFromEmail function

---
+++ pallida-basic-exam-trial/namefromemail/NameFromEmail/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace NameFromEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes an email address as input in the following format: firstName.lastName@exam.com
+            //and returns a string that represents the user name in the following format: lastName firstName
+            //example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
+            //accents does not matter 
+            NameFromEmail();
+            Console.ReadLine();
+        }
+        private static void NameFromEmail()
+        {
+            Console.WriteLine("Please enter your email address in the following format: firstName.lastName@exam.com");
+            string email = Console.ReadLine();
+            string user = email.Substring(0, email.IndexOf("@"));
+
+            string firstName = user.Substring(0, user.IndexOf("."));
+            string lastName = user.Substring(user.IndexOf(".")+1);
+
+
+            Console.WriteLine(lastName +" "+ firstName);
+
+        }
+    }
+}
