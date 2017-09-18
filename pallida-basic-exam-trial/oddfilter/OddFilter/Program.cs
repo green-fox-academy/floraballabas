d669de5b4c5aa578efc8e07a03d10f1eb132a9f1
Flora <floraballabas@gmail.com>
2017-09-18 10:23:32 +0200

    Created the Oddfilter function with for loop and if statement

---
+++ pallida-basic-exam-trial/oddfilter/OddFilter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an OddFilter function that takes a list as a parameter,
+            // and returns a new list with every odd element from the orignal list
+
+            // In case of the example input above, the given PrintList function should print 1 3 5 
+            PrintList();
+
+            Console.ReadLine();
+        }
+        public static void OddFilter()
+        {
+            var toBeSorted = new List<int> { 1, 2, 3, 4, 5 };
+            var oddElements = new List<int>();
+
+            for (int i = 0; i < toBeSorted.Count; i++)
+            {
+                if (toBeSorted[i] % 2 != 0)
+                {
+                    oddElements.Add(toBeSorted[i]);
+                }
+            }
+            foreach (int odd in oddElements)
+            {
+                Console.Write(odd);
+            }
+        }
+        public static void PrintList()
+        {
+            foreach (int odd in )
+            {
+                Console.Write("{0} ", element);
+            }
+        }
+    }
+}
+
--- week-02/day-05/Palindrome Searcher task/PalindromeSearcher/Program.cs
+++ week-02/day-05/Palindrome Searcher task/PalindromeSearcher/Program.cs
@@ -4,9 +4,30 @@ namespace PalindromeSearcher
 {
     class Program
     {
+        public static void SearchPalindrom()
+        {
+            //Create a function named search palindrome following your current language's style guide. 
+            //It should take a string, search for palindromes that at least 3 characters long and return a list with the found palindromes.
+            Console.WriteLine("Please enter a string");
+            string input = Console.ReadLine();
+
+            char[] array = input.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                //if (array[i] != array[array.Length - 1 -i])
+                //{
+                //  return false;
+                //}
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            SearchPalindrom();
+            Console.ReadLine();
         }
     }
 }
