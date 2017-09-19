7ad5134aedc9060e3db396260f734062ef6d084e
Flora <floraballabas@gmail.com>
2017-09-19 11:50:26 +0200

    Uploaded the CountLines exercise

---
+++ week-03/CountLines/CountLines/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+
+            Console.WriteLine(CountTheLines());
+            Console.ReadLine();
+        }
+        public static int CountTheLines()
+        {
+            string path = @"./assets/my-file.txt";
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                return content.Length;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+
+        }
+    }
+}
