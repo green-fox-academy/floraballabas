920d6bf6cbafcb7171544ec23f2cbd4f3b793e6c
Flora <floraballabas@gmail.com>
2017-09-19 20:26:03 +0200

    Rearranged my folder section

---
+++ week-03/day-02/CopyFile/CopyFileApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CopyFileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
+            CopyFile();
+            Console.ReadLine();
+        }
+
+        private static void CopyFile()
+        {
+            string path = "../../original.txt";
+            string destination = "copied.txt";
+
+            File.Copy(path, destination);
+
+        }
+    }
+}
