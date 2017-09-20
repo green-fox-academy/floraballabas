adcc06a4a05510caf966a008f29f43102f36e1e9
Flora <floraballabas@gmail.com>
2017-09-20 15:52:40 +0200

    Solved the Checkerboard task with for loops and if statements

---
+++ week-03/day-03/Checkerboard/CheckerBoard/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace CheckerBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // fill the canvas with a checkerboard pattern.
+
+            double x = 35;
+            for (int i = 0; i < 8; i++)
+            {
+                double startPoint = 0+i*35;
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            foxDraw.FillColor(Colors.White);
+                        }
+                        else
+                        {
+                            foxDraw.FillColor(Colors.Black);
+                        }
+                    }
+                    else 
+                    {
+                        if (j % 2 == 0)
+                        {
+                            foxDraw.FillColor(Colors.Black);
+                        }
+                        else
+                        {
+                            foxDraw.FillColor(Colors.White);
+                        } 
+                    }
+                    foxDraw.DrawRectangle(0 + 35 * j, startPoint, x, x);
+                }
+            }
+        }
+    }
+}
