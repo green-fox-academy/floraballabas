b176d51fd2dfd2b7b7339cf6ae605896e41368f7
Flora <floraballabas@gmail.com>
2017-09-20 11:46:26 +0200

    Uploaded the To the Center task

---
+++ week-03/day-03/Middlelines/ToTheCenter/ToTheCenter/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace ToTheCenter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // draw 3 lines with that function.
+            LineDrawing();
+
+        }
+        public void LineDrawing()
+        {
+            var foxDraw = new FoxDraw(canvas);
+            for (int i = 1; i < 4; i++)
+            {
+                double x = 10 + i * 50;
+                double y = 10 + i * 30;
+                var StartPoint = new Point(x, y);
+                var centerPoint = new Point(150, 150);
+                foxDraw.DrawLine(StartPoint, centerPoint);
+            }
+        }
+    }
+}
