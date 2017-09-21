61d38a3162d014129e1b824af9bf1f441626e7b9
Flora <floraballabas@gmail.com>
2017-09-21 11:45:09 +0200

    Uploaded the Bunnies task

--- week-03/day-04/Bunnies/Bunnies/Program.cs
+++ week-03/day-04/Bunnies/Bunnies/Program.cs
@@ -10,6 +10,17 @@ namespace Bunnies
     {
         static void Main(string[] args)
         {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+            Console.WriteLine(Recursive(2));
+            Console.ReadLine();
+        }
+
+        private static int Recursive(int bunny)
+        {
+            if (bunny == 0)
+                return 0;
+            return 2 + Recursive(bunny-1);
         }
     }
 }
