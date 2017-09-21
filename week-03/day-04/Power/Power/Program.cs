5ac520aca0620326039b8b5fa9b86d9b964c671f
Flora <floraballabas@gmail.com>
2017-09-21 10:59:02 +0200

    Uploaded the Power task

---
+++ week-03/day-04/Power/Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+            Console.WriteLine(Recursive(2));
+            Console.ReadLine();
+        }
+
+        private static int Recursive(int n)
+        {
+            int b = 3;
+            if (n == 0)
+                return 1;
+            return b * Recursive(n - 1);
+        }
+    }
+}
