5c7b7e4f1c87a7b74236f98b38953c1ea28e69ab
Flora <floraballabas@gmail.com>
2017-09-21 14:12:28 +0200

    Uploaded the Strings task

--- week-03/day-04/BunniesAgain/BunniesAgain/Program.cs
+++ week-03/day-04/BunniesAgain/BunniesAgain/Program.cs
@@ -14,7 +14,7 @@ namespace BunniesAgain
             // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
             // have 3 ears, because they each have a raised foot. Recursively return the
             // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
-            Console.WriteLine(Recursive(6));
+            Console.WriteLine(Recursive(12));
             Console.ReadLine();
         }
 
