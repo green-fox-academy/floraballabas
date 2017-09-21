213ca7fae9efe78f86f6590f29cdeee2c03bd2e6
Flora <floraballabas@gmail.com>
2017-09-21 16:34:09 +0200

    Uploaded the Strings Again again task

---
+++ week-03/day-04/Strings3/Strings3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+            string input = "kiscica";
+            Console.WriteLine(Recursive(input));
+            Console.ReadLine();
+        }
+
+        private static string Recursive(string input)
+        {
+            if (input.Length == 1)
+                return input;
+            else
+                return input[0] + "*" + Recursive(input.Substring(1)); 
+        }
+    }
+}
