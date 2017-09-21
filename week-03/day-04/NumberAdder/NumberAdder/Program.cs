cd50cd0c4fc692837cdb921a8f6cec0012eda4cf
Flora <floraballabas@gmail.com>
2017-09-21 10:20:14 +0200

    Uploaded the NumberAdder task

---
+++ week-03/day-04/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+            Console.WriteLine(Recursive(18));
+            Console.ReadLine();
+        }
+
+        private static int Recursive(int n)
+        {
+            if (n == 1)
+                return 1;
+            return n + Recursive(n -1);
+            
+        }
+    }
+}
