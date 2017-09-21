131371ec5e9076d70f2d160e9a37c3828364136a
Flora <floraballabas@gmail.com>
2017-09-21 16:10:14 +0200

    Uploaded the strings again task

---
+++ week-03/day-04/Strings2/Strings2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+            string input = "xaxxxannaxxa";
+            int length = input.Length;
+            Console.WriteLine(Recursive(input, length));
+            Console.ReadLine();
+        }
+
+        private static string Recursive(string input, int length)
+        {
+            string result;
+
+            if (length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (input.Substring(length - 1, 1) == "x")
+                {
+                    result = "";
+                    return Recursive(input, length - 1) + result;
+                }
+                else
+                {
+                    return Recursive(input, length - 1) + input.Substring(length - 1, 1);
+                }
+            }
+        }
+    }
+}
