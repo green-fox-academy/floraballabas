164b95da08ccf6b39e14775257fd84f41df136f3
Flora <floraballabas@gmail.com>
2017-09-21 10:12:22 +0200

    Uploaded The Counter task

---
+++ week-03/day-04/Counter/Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Recursive(20);
+            Console.ReadLine();
+        }
+
+        private static void Recursive(int n)
+        {
+            if (n < 0)
+                return;
+            Console.WriteLine(n);
+            Recursive(n - 1);
+        }
+    }
+}
