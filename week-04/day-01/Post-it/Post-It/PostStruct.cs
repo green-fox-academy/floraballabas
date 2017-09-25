0bced23ee673a5d43c5ed4dc5dc9bd7ff53c5b41
Flora <floraballabas@gmail.com>
2017-09-25 13:24:05 +0200

    Uploaded the Post It task

---
+++ week-04/day-01/Post-it/Post-It/PostStruct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_It
+{
+    struct PostStruct
+    {
+        public string BackgroundColor;
+        public string Text;
+        public string Textcolor;
+    }
+}
---
+++ week-04/day-01/Post-it/Post-It/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post_It
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostStruct poststruct1;
+            PostStruct poststruct2;
+            PostStruct poststruct3;
+
+            poststruct1.BackgroundColor = "orange";
+            poststruct1.Text = "New idea";
+            poststruct1.Textcolor = "black";
+
+            poststruct2.BackgroundColor = "pink";
+            poststruct2.Text = "Awesome";
+            poststruct2.Textcolor = "blue";
+
+            poststruct3.BackgroundColor = "yellow";
+            poststruct3.Text = "Superb";
+            poststruct3.Textcolor = "green";
+        }
+    }
+}
