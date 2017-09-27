5063490e79e14af715cf50c6de71a549c1056325
Flora <floraballabas@gmail.com>
2017-09-27 16:01:16 +0200

    Created test for CounterLetter

---
+++ week-04/day-03/CountLetter/CountLetter/CountApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetter
+{
+    public class CountApp
+    {
+        public Dictionary<char, int> LetterCounter(string input)
+        {
+            Dictionary<char, int> letterDictionary = new Dictionary<char, int>();
+            char[] inputArray = input.ToCharArray();
+            
+            foreach (var character in inputArray)
+            {
+                if (!letterDictionary.ContainsKey(character))
+                {
+                    letterDictionary.Add(character, 1);
+                }
+                else
+                {
+                    letterDictionary[character]++;
+                }
+            }
+            return letterDictionary;
+        }
+    }
+}
---
+++ week-04/day-03/CountLetter/CountLetterTest/CountAppTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountLetter;
+
+namespace CountLetterTest
+{
+    [TestFixture]
+    public class CountAppTest
+    {
+        CountApp myClass = new CountApp();
+        [Test]
+        public void CounterTest()
+        {
+            string input = "macska";
+
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+                { 'm', 1},
+                { 'a', 2},
+                { 'c', 1},
+                { 's', 1},
+                { 'k', 1}
+            };
+
+            Assert.AreEqual(output, myClass.LetterCounter(input));
+        }
+    }
+}
