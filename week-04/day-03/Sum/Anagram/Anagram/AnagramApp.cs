bca18be23c4ced550ebfa2dfdcdd57dc2ed4ea75
Flora <floraballabas@gmail.com>
2017-09-27 14:38:01 +0200

    Created test for Anagram exercise

---
+++ week-04/day-03/Sum/Anagram/Anagram/AnagramApp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class AnagramApp
+    {
+        public bool IsAnagram(string input1, string input2)
+        {
+            input1 = input1.ToLower();
+            input2 = input2.ToLower();
+
+            //Turn strings into arrays and then sort them
+            char[] array1 = input1.ToCharArray();
+            char[] array2 = input2.ToCharArray();
+            Array.Sort(array1);
+            Array.Sort(array2);
+
+            //Turn them back to strings
+            string sorted1 = new string(array1);
+            string sorted2 = new string(array2);
+
+            //Check with if statement if they are the same
+            if (sorted1 == sorted2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ week-04/day-03/Sum/Anagram/AnagramTest/AnagramAppTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class AnagramAppTest
+    {
+        AnagramApp myClass = new AnagramApp();
+        [Test]
+        public void IsAnagramTest()
+        {
+            string input1 = "alma";
+            string input2 = "mala";
+
+            bool output = true;
+            Assert.AreEqual(output,myClass.IsAnagram(input1, input2));
+        }
+    }
+}
