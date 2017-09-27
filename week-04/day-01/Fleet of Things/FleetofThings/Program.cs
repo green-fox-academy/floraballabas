6cfb919457ec784f4d2eb83ceef05ae929516c7d
Flora <floraballabas@gmail.com>
2017-09-27 21:54:40 +0200

    Solved the FleetOfThings task - print method was replaced by CW in the Thing class

---
+++ week-04/day-01/Fleet of Things/FleetofThings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    public class FleetOfThings
+    {
+        public static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+
+            Console.Write("1. ");
+            fleet.Add(new Thing("Get milk"));
+            Console.Write("2. ");
+            fleet.Add(new Thing("Remove the obstacles"));
+            Console.Write("3. ");
+            fleet.Add(new Thing("Stand up"));
+            Console.Write("4. ");
+            fleet.Add(new Thing("Eat lunch"));
+            Console.ReadLine();
+        }
+        public void Print(List<Thing> Things)
+        {
+            Console.WriteLine(Things);
+        }
+    }
+}
---
+++ week-04/day-01/Fleet of Things/FleetofThings/Thing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    public class Thing
+    {
+        private string Name;
+        private bool Completed;
+
+        public Thing(string name)
+        {
+            this.Name = name;
+            if (name == "Stand up" || name == "Eat lunch")
+            {
+                Complete();
+                Console.WriteLine("[X] "+ this.Name);
+            }
+            else
+            {
+                Completed = false;
+                Console.WriteLine("[ ] "+this.Name);
+            }
+        }
+
+        public void Complete()
+        {
+            this.Completed = true;
+        }
+    }
+}
