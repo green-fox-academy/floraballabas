99d9e8ac16f0196b7d4f549ba7f7dddfa274386c
Flora <floraballabas@gmail.com>
2017-09-27 14:03:50 +0200

    Created several tests for the Sum task

---
+++ week-04/day-03/Sum/Sum/Sum/SumApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class SumApp
+    {
+        public int GetSum(List<int> sumList)
+        {
+            int sum = 0;
+            if (sumList != null)
+            {
+                foreach (int number in sumList)
+                {
+                    sum += number;
+                }
+                return sum;
+            }
+            return 0;
+        }
+    }
+}
---
+++ week-04/day-03/Sum/SumTest/SumAppTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SumAppTest
+    {
+        SumApp myClass = new SumApp();
+        [Test]
+        public void GetSumTestEmpty()
+        {
+            //arrange
+            List<int> sumList = new List<int>();
+            //act
+            int output = 0;
+            //assert
+            Assert.AreEqual(output, myClass.GetSum(sumList));
+        }
+        [Test]
+        public void GetSumTest1()
+        {
+            //arrange
+            List<int> sumList = new List<int>() {5};
+            //act
+            int output = 5;
+            //assert
+            Assert.AreEqual(output, myClass.GetSum(sumList));
+        }
+        [Test]
+        public void GetSumTestMultiple()
+        {
+            //arrange
+            List<int> sumList = new List<int>() {1,2,3};
+            //act
+            int output = 6;
+            //assert
+            Assert.AreEqual(output, myClass.GetSum(sumList));
+        }
+        [Test]
+        public void GetSumTestNull()
+        {
+            //arrange
+            List<int> sumList = new List<int>();
+            //act
+            int output = 0;
+            //assert
+            Assert.AreEqual(output, myClass.GetSum(null));
+        }
+
+    }
+}
