8d13697fe7fc8932d8ec2096746222a43f883e77
Flora <floraballabas@gmail.com>
2017-10-16 11:27:19 +0200

    Created two tests to see if card value is valid

---
+++ week-06/day-01/Poker/Poker/Cards.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Cards
+    {
+        private string card;
+        private bool IsValid;
+
+        public Cards(string cardInput)
+        {
+            if (IsItAcceptable(cardInput))
+            {
+                this.card = cardInput;
+            }
+            else
+            {
+                this.card = "6D";
+            }
+        }
+
+        public bool IsItAcceptable(string cardInput)
+        {
+            if (cardInput.Count() == 2)
+            {
+                IsValid = true;
+            }
+            else
+            {
+                IsValid = false;
+            }
+            return IsValid;
+        }
+    }
+}
---
+++ week-06/day-01/Poker/PokerTest/CardTestClass.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Poker;
+
+namespace PokerTest
+{
+    [TestFixture]
+    class CardTestClass
+    {
+        [Test]
+        public void TestWithString()
+        {
+            string expected = "2H";
+            var cardAtHand = new Cards(expected);
+            Assert.IsTrue(cardAtHand.IsItAcceptable(expected));
+        }
+        [Test]
+        public void TestWithTooLongString()
+        {
+            string expected = "2Hkj";
+            var cardAtHand = new Cards(expected);
+            Assert.IsFalse(cardAtHand.IsItAcceptable(expected));
+        }
+    }
+}
