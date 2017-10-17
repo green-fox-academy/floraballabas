7b77a802bb196e196be330e2e17aff24788a20c3
Flora <floraballabas@gmail.com>
2017-10-17 17:04:47 +0200

    Created the IComparable interface

---
+++ week-06/day-02/Comparable/Comparable/Domino.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Comparable
+{
+    public class Domino : IComparable<Domino>
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public string GetDominoString()
+        {
+            return $"[{Values[0]}, {Values[1]}]";
+        }
+
+        public int CompareTo(Domino that)
+        {
+            if (this.Values[0] > that.Values[0]) return 1;
+            if (this.Values[0] == that.Values[0]) return 0;
+            return -1;
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
---
+++ week-06/day-02/Comparable/Comparable/Dominoes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparable
+{
+    public class Dominoes
+    {
+        static void Main(string[] args)
+        {
+
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+
+            dominoes.Sort();
+            foreach (var domino in dominoes)
+            {
+                Console.WriteLine(domino.GetDominoString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
