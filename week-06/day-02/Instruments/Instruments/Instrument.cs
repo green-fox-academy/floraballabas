1669fcb5e12d0dd592bc5879e813f3a5e361af35
Flora <floraballabas@gmail.com>
2017-10-17 17:41:43 +0200

    Created the two abstract classes

---
+++ week-06/day-02/Instruments/Instruments/Instrument.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    abstract class Instrument
+    {
+        protected string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public abstract void Play();
+    }
+}
---
+++ week-06/day-02/Instruments/Instruments/StringedInstrument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instruments
+{
+    abstract class StringedInstrument : Instrument
+    {
+        protected int numberOfStrings;
+
+        public abstract void Sound();
+
+        public override void Play()
+        {
+            Console.WriteLine("{0}, a {1}-stringed instrument that {2}s");
+        }
+    }
+}
