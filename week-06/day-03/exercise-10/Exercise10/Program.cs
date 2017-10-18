501e49937b9ee9e8b4df3bcf673f3982371ca1fd
Flora <floraballabas@gmail.com>
2017-10-18 16:20:53 +0200

    query syntax created

--- week-06/day-03/exercise-10/Exercise10/Program.cs
+++ week-06/day-03/exercise-10/Exercise10/Program.cs
@@ -10,6 +10,37 @@ namespace Exercise10
     {
         static void Main(string[] args)
         {
+            //Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes, 
+            //it's up to you how you name/create them! Write a LINQ Expression to find the foxes with green color! 
+            //Write a LINQ Expression to find the foxes with green color and pallida type!
+            var listOfFoxes = new List<Fox>();
+            var fox1 = new Fox("Alpaga", "green", "pallida");
+            var fox2 = new Fox("Totoro", "green", "static");
+            var fox3 = new Fox("FutureFox", "yellow", "FutureType");
+            listOfFoxes.Add(fox1);
+            listOfFoxes.Add(fox2);
+            listOfFoxes.Add(fox3);
+
+            //Query for green foxes
+            var listOfFoxesQuery = from fox in listOfFoxes
+                                   where fox.Color == "green"
+                                   select fox;
+            foreach (var fox in listOfFoxesQuery)
+            {
+                Console.WriteLine("The green foxes are called: {0}", fox.Name);
+            }
+
+            //Query for green foxes that have pallida type
+            var listOfFoxesQuery2 = from fox in listOfFoxes
+                                   where fox.Color == "green" && fox.Type == "pallida"
+                                   select fox;
+
+            foreach (var fox in listOfFoxesQuery2)
+            {
+                Console.WriteLine("The green foxes that have pallida type are called: {0}", fox.Name);
+            }
+
+            Console.ReadLine();
         }
     }
 }
