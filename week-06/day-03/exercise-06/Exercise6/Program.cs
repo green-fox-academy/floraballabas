544cbf3a58c813deba28ce7c49a6ba9e1ca6e960
Flora <floraballabas@gmail.com>
2017-10-18 14:42:07 +0200

    Solved task6 with query and method syntax as well

---
+++ week-06/day-03/exercise-06/Exercise6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of characters in a given string!
+            string toBeChecked = "kiscica";
+
+            //Query Syntax
+            var frequencyOfCharacters = from character in toBeChecked
+                                        group character by character into uniqueChars
+                                        select new { uniqueChars.Key, Count = uniqueChars.Count() };
+            foreach (var character in frequencyOfCharacters)
+            {
+                Console.WriteLine("Letter {0} appears {1} times in the string", character.Key, character.Count);
+            }
+
+            Console.WriteLine();
+
+            //Method syntax
+            var frequencyOfCharactersWithLambda = toBeChecked.GroupBy(character => character).ToDictionary
+                (character => character.Key, character => character.Count());
+            foreach (var character in frequencyOfCharactersWithLambda)
+            {
+                Console.WriteLine("Letter {0} appears {1} times in the string", character.Key, character.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
