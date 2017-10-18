32ae595b7cc8566e54608092ac96d956507bc77d
Flora <floraballabas@gmail.com>
2017-10-18 13:46:46 +0200

    Solved task5 with method syntax

---
+++ week-06/day-03/exercise-05/Exercise5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of numbers in the following array:
+
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var freqQueryWithLambda = n.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            foreach (var query in freqQueryWithLambda)
+            {
+                Console.WriteLine(query);
+            }
+            Console.ReadLine();
+        }
+    }
+}
