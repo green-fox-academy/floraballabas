8dfa56748e019ef18fa7e7554a6755559323452e
Flora <floraballabas@gmail.com>
2017-10-18 11:41:47 +0200

    Solved with Query syntax

---
+++ week-06/day-03/exercise-02/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var oddQuery = (from odd in n
+                            where odd % 2 == 1 || odd % 2 == -1
+                            select odd).Average();
+
+            Console.WriteLine(oddQuery);
+            Console.ReadLine();
+        }
+    }
+}
