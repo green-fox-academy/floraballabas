2b8a2ab1e79c4d31a5732c1d02a09de22616024e
Flora <floraballabas@gmail.com>
2017-10-18 17:01:53 +0200

    Added method syntax as well to task3

--- week-06/day-03/exercise-03/Exercise3/Program.cs
+++ week-06/day-03/exercise-03/Exercise3/Program.cs
@@ -13,13 +13,20 @@ namespace Exercise3
             //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
 
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
-
-            var scoreQuery = (from square in n
+            //Query syntax
+            var scoreQuery = from square in n
                              where square > 0
-                             select square);
+                             select Math.Pow(square, 2);
             foreach (var squarenum in scoreQuery)
             {
-                Console.WriteLine(Math.Pow(squarenum, 2));
+                Console.WriteLine(squarenum);
+            }
+
+            //Method syntax
+            var scoreQueryWithLambda = n.Where(square => square > 0).Select(square => Math.Pow(square, 2));
+            foreach (var squarenum in scoreQueryWithLambda)
+            {
+                Console.WriteLine(squarenum);
             }
             Console.ReadLine();
         }
