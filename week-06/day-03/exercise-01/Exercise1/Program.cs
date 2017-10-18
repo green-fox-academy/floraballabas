1a320dd945b02e465ff341cfb68a27f60f4f21e7
Flora <floraballabas@gmail.com>
2017-10-18 11:18:53 +0200

    Uploaded the first exercise, solved with LINQ

---
+++ week-06/day-03/exercise-01/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the even numbers from the following array:
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenQuery = from num in n
+                                         where num % 2 ==0
+                                         select num;
+            foreach (var evenNumber in evenQuery)
+            {
+                Console.WriteLine(evenNumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
