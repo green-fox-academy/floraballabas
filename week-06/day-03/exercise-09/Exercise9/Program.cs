ae5c821ef389c0428dd78a71ae1194ddf0d751b8
Flora <floraballabas@gmail.com>
2017-10-18 15:46:10 +0200

    created two method syntax solutions for task9

---
+++ week-06/day-03/exercise-09/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to convert a char array to a string!
+            char[] letterArray = {'c', 'i', 'c', 'a'};
+
+            //Query syntax
+
+            //Method syntax
+            //var toStringQueryWithLambda = String.Join("", letterArray.Select(character => character.ToString()));
+            var toStringQueryWithLambda = letterArray.Aggregate(new StringBuilder(), (a, b) => a.Append(b));
+            Console.WriteLine(toStringQueryWithLambda);
+
+            Console.ReadLine();
+        }
+    }
+}
