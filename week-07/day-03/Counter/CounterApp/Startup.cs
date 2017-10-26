d0320e0ae6e9475c03ecee406201dcd42b1d1f9f
Flora <floraballabas@gmail.com>
2017-10-26 13:30:55 +0200

    Counterapp ready

--- week-07/day-03/Counter/CounterApp/Startup.cs
+++ week-07/day-03/Counter/CounterApp/Startup.cs
@@ -17,19 +17,20 @@ namespace CounterApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<Models.Counter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
-            app.UseMvcWithDefaultRoute();
-
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
