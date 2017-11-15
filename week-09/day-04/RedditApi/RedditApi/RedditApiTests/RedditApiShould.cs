54cb1d740c08fa71e7dcc05d67669be7fe4239f4
floraballabas <floraballabas@gmail.com>
2017-11-15 14:15:42 +0100

    two working tests added that check the status code

---
+++ week-09/day-04/RedditApi/RedditApi/RedditApiTests/RedditApiShould.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using RedditApi;
+using RedditApi.Models;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedditApiTests
+{
+    public class RedditApiShould
+    {
+        private readonly TestServer Server;
+        private readonly HttpClient Client;
+
+        public RedditApiShould()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnOkStatusForListsPosts()
+        {
+            var response = await Client.GetAsync("/posts");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReturnOkStatusForAddPosts()
+        {
+            var usedPost = new Post
+            {
+                Title = "Test",
+                Url = "test.hu",
+                Score = 8
+            };
+
+            var convertedUsedPost= JsonConvert.SerializeObject(usedPost);
+            var data = new StringContent(convertedUsedPost, encoding: Encoding.UTF8, mediaType: "application/json");
+            var response = await Client.PostAsync("/posts", data);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
